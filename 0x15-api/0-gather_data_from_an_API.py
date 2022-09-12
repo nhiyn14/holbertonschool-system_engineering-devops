444e3296e03316e78fb0f3b1f92ecd9e18bd53b6
Cienna Nguyen <4326@holbertonschool.com>
2022-09-12 16:45:51 +1000

    Add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" for a given employee ID, returns his/her TODO list progress"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    """function to get employees todo list"""
+    # Retrieve the employee infor + todo lists. Then convert to json
+    employee_id = int(argv[1])
+    employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                            format(employee_id)).json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(employee_id)).json()
+
+    # Make a list of done tasks
+    done_task = []
+    for task in todo:
+        if task['completed'] is True:
+            done_task.append(task.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee.get('name'), len(done_task), len(todo)))
+    for task in done_task:
+        print("\t {}".format(task))
